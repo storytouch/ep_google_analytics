8ec75ca0cf095f5e020fea140b51b4e04e4d584c
Luiza Pagliari <lpagliari@gmail.com>
2017-05-19 18:46:33 -0300

    Upgrate from ga.js to Universal Analytics
    As per https://support.google.com/analytics/answer/4457764?hl=en, ga.js
    is being deprecated, so we started using Universal Analytics according
    to the migration guide on
    https://developers.google.com/analytics/devguides/collection/upgrade/reference/gajs-analyticsjs#snippet

--- index.js
+++ index.js
@@ -9,18 +9,16 @@ if(settings.ep_googleanalytics){ // Setup testing else poop out
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
   if(gaCode){
-    var gaString = "<script type='text/javascript'>var _gaq = _gaq || [];_gaq.push(['_setAccount', '"+gaCode+"']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>";
+    var gaString = eejs.require('ep_googleanalytics/templates/analytics.ejs', {gaCode: gaCode});
   }
   else{ // gaCode isn't set
     var gaString = "<script>alert('ep_googleanalytics.gaCode not set in settings.json, insert it in /admin/settings')</script>";
   }
 
 /* Leaving this in here for sanity */
-//  gaString += "<script type='text/javascript'>function sendGA(this){_gaq.push(['_trackEvent', this, 'Click', document.domain]);alert('sending to GA');}/* Google Analytics error catching */window.onerror = function(message, file, lineNumber) {_gaq.push(['_trackEvent','error',file + ':' + lineNumber,message + '']);};alert('test');$('div').bind('click', sendGA(this));$('ul').bind('click', sendGA(this));$('li').bind('click', alert('fu');sendGA(this));";
+//  gaString += "<script type='text/javascript'>function sendGA(this){ga('send','event', this, 'Click', document.domain]);alert('sending to GA');}/* Google Analytics error catching */window.onerror = function(message, file, lineNumber) {ga('send','event','error',file + ':' + lineNumber,message + '']);};alert('test');$('div').bind('click', sendGA(this));$('ul').bind('click', sendGA(this));$('li').bind('click', alert('fu');sendGA(this));";
 //  gaString += "<script type='text/javascript'>$('a').bind('click', alert('wut'));</script>";
 
   args.content = args.content + gaString; // add Google Analytics to the contents
   return cb();
 }
-
-
