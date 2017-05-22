0923074ce73500ca70d7533b1337cbcb7b155249
Luiza Pagliari <lpagliari@gmail.com>
2017-05-22 07:00:46 -0300

    Upgrate to Universal Analytics
    As per https://support.google.com/analytics/answer/4457764?hl=en, ga.js
    is being deprecated, so we started using Universal Analytics according
    to the migration guide on
    https://developers.google.com/analytics/devguides/collection/upgrade/reference/gajs-analyticsjs#snippet
    
    Keep the option to use ga.js, to not break compatibility.

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # ep_google_analytics
 
-Add Google Analytics to Etherpad Lite
+Add Google Analytics to Etherpad Lite. Keep track of pageview events by default.
 
 **IMPORTANT**: Add the following to your settings.json file:
+```
+"ep_googleanalytics": {
+  "trackingCode": "UA-1234567-8"
+}
+```
+
+*Notice*: the original version of ep_googleanalytics uses the deprecated `ga.js`. So if your Etherpad instance has legacy code using `gaq.push(...)` -- on other plugins, for example --, you need to make sure ep_googleanalytics loads `ga.js` instead. To do that, use the setting `gaCode` instead of `trackingCode`:
 ```
 "ep_googleanalytics": {
   "gaCode": "UA-1234567-8"
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 var eejs = require('ep_etherpad-lite/node/eejs/');
 var settings = require('ep_etherpad-lite/node/utils/Settings');
 
-if(settings.ep_googleanalytics){ // Setup testing else poop out
+if(settings.ep_googleanalytics) {
   var gaCode = settings.ep_googleanalytics.gaCode;
-}else{
-  var gaCode = false;
+  var trackingCode = settings.ep_googleanalytics.trackingCode;
 }
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-  if(gaCode){
-    var gaString = "<script type='text/javascript'>var _gaq = _gaq || [];_gaq.push(['_setAccount', '"+gaCode+"']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>";
+  if(trackingCode) { // using analytics.js
+    var gaString = eejs.require('ep_googleanalytics/templates/analytics.js.ejs', {trackingCode: trackingCode});
+
+    /* Leaving this in here for sanity */
+    //  gaString += "<script type='text/javascript'>function sendGA(this){ga('send','event', this, 'Click', document.domain]);alert('sending to GA');}/* Google Analytics error catching */window.onerror = function(message, file, lineNumber) {ga('send','event','error',file + ':' + lineNumber,message + '']);};alert('test');$('div').bind('click', sendGA(this));$('ul').bind('click', sendGA(this));$('li').bind('click', alert('fu');sendGA(this));";
+    //  gaString += "<script type='text/javascript'>$('a').bind('click', alert('wut'));</script>";
+  }
+  else if(gaCode) { // using ga.js (deprecated)
+    var gaString = eejs.require('ep_googleanalytics/templates/ga.js.ejs', {gaCode: gaCode});
+
+    /* Leaving this in here for sanity */
+    //  gaString += "<script type='text/javascript'>function sendGA(this){_gaq.push(['_trackEvent', this, 'Click', document.domain]);alert('sending to GA');}/* Google Analytics error catching */window.onerror = function(message, file, lineNumber) {_gaq.push(['_trackEvent','error',file + ':' + lineNumber,message + '']);};alert('test');$('div').bind('click', sendGA(this));$('ul').bind('click', sendGA(this));$('li').bind('click', alert('fu');sendGA(this));";
+    //  gaString += "<script type='text/javascript'>$('a').bind('click', alert('wut'));</script>";
   }
   else{ // gaCode isn't set
-    var gaString = "<script>alert('ep_googleanalytics.gaCode not set in settings.json, insert it in /admin/settings')</script>";
+    var gaString = "<script>alert('ep_googleanalytics.trackingCode not set in settings.json, insert it in /admin/settings')</script>";
   }
-
-/* Leaving this in here for sanity */
-//  gaString += "<script type='text/javascript'>function sendGA(this){_gaq.push(['_trackEvent', this, 'Click', document.domain]);alert('sending to GA');}/* Google Analytics error catching */window.onerror = function(message, file, lineNumber) {_gaq.push(['_trackEvent','error',file + ':' + lineNumber,message + '']);};alert('test');$('div').bind('click', sendGA(this));$('ul').bind('click', sendGA(this));$('li').bind('click', alert('fu');sendGA(this));";
-//  gaString += "<script type='text/javascript'>$('a').bind('click', alert('wut'));</script>";
 
   args.content = args.content + gaString; // add Google Analytics to the contents
   return cb();
 }
-
-
